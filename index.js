5ac63d4533d2059d1f518cf8dc5c32df5851459e
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-12-10 12:19:10 +0500

    setup mongodb connection

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ const books = require('./src/router/books')
 app.use('/users', users)
 app.use('/books', books)
 
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb+srv://admin:admin@cluster0.uvfwj5u.mongodb.net/auth", {
+    useNewurlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("DB Connected....")
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello World....");
+});
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
